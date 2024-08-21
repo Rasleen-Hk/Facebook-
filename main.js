bdfb90ba0ade12957bc9b2cf5e3a11caa20ce987
Rasleen Haj khlifa <rasleenhajh@gmail.com>
2024-08-21 05:57:24 +0100

    The JS file

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let bars = document.getElementById("bars");
+
+bars.addEventListener("click",()=>{
+    window.location.replace('bars.html');
+})
+
+
+function change(element){
+    element.style.color ="blue";
+};
