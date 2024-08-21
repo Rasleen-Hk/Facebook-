8ed6430cd469e01b38a6d6cc2eca3dcaa60286f6
Rasleen Haj khlifa <rasleenhajh@gmail.com>
2024-08-21 05:58:45 +0100

    The js file number 2

---
+++ bars.js
@@ -0,0 +1,14 @@
+
+
+function change(){
+    window.location.replace("index.html");
+}
+
+
+function Callalert(){
+    alert("are you sure you want to log out?")
+    setTimeout(function (){
+    window.location.replace("logout.html");
+},1000);
+}
+
